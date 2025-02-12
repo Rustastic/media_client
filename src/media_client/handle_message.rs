6e0665e06107a8010c63bf599a7dc5e143833bfd
kGB <116108379+gibosio@users.noreply.github.com>
2025-02-12 17:18:55 +0100

    fix

--- src/media_client/handle_message.rs
+++ src/media_client/handle_message.rs
@@ -90,7 +90,7 @@ impl MediaClient {
                 }
             }
             Media(media_id, content) => {
-                println!("[MediaClient] received media: {media_id}");
+                println!("[MediaClient {} ] received media: {media_id}", self.id);
                 self.file_assembler.add_media_file(&media_id, content);
             }
             _ => (),
